c7e35c4cb7f05d607f9c7ebf1c5361fe8e654d14
IgorAvilaPereira <igor.pereira@riogrande.ifrs.edu.br>
2016-03-23 22:14:21 -0300

    Adicionando projeto decorator que dá desconto de acordo com o numero de condimentos

---
+++ 220316Decorator_Opcao2/src/negocio/Bebida.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package negocio;
+
+/**
+ *
+ * @author iapereira
+ */
+public abstract class Bebida {
+
+    protected String descricao;
+    protected double quantidade;
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public double custoTotal() {
+        if (this.quantidade == 2) {
+            return (this.custo()) * 0.6;
+        }
+        if (this.quantidade == 3) {
+            return (this.custo()) * 0.4;
+        }
+        if (this.quantidade > 3) {
+            return (this.custo()) * 0.5;
+        }
+        // fazer para o restante dos descontos....
+        return 0.0;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public abstract double custo();
+
+}
